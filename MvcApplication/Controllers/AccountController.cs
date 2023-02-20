fc4935ef1a8a160835a1457d2a7daf6fc82b5350
Tamilanban Chinnaiyan <anbantamizh@gmail.com>
2023-02-20 12:57:10 +1030

    Initial commit

---
+++ MvcApplication/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public ActionResult Login(string returnUrl)
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties
+            {
+                RedirectUri = returnUrl ?? Url.Action("Index", "Home")
+            },
+                "Auth0");
+            return new HttpUnauthorizedResult();
+        }
+
+        [Authorize]
+        public void Logout()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            HttpContext.GetOwinContext().Authentication.SignOut("Auth0");
+        }
+
+
+        [Authorize]
+        public ActionResult Claims()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<ActionResult> sloLogout()
+        {
+            Logout();
+            return View();
+        }
+    }
+}
---
+++ MvcApplication/Controllers/Auth0AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class Auth0AccountController : Controller
+    {
+         
+        [Authorize]
+        public void Logout()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            HttpContext.GetOwinContext().Authentication.SignOut("Auth0");
+        }
+         
+    }
+}
